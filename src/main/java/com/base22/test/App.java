d90442c46a80d95e32cad252c1ec52ca18a1edc2
Margarita Hernandez <margara_ha@hotmail.com>
2016-12-28 18:10:12 -0600

    Added producer

--- src/main/java/com/base22/test/App.java
+++ src/main/java/com/base22/test/App.java
@@ -2,12 +2,12 @@ package com.base22.test;
 
 /**
  * Hello world!
- *
  */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
-    }
+public class App {
+	public static void main( String[] args ) {
+		MyKafkaProducer producer = new MyKafkaProducer();
+		producer.runProducer();
+
+	}
+
 }
